ec1d399f055e211a9280cffd26a301a5fe1864a0
Ryan Allen <rcallen89@gmail.com>
2019-11-25 09:08:20 -0700

    Add supply hash to node Add method to combine all supplies in Linked List Add test for supply hash outcome

--- lib/node.rb
+++ lib/node.rb
@@ -2,9 +2,10 @@
 
 class Node
   attr_reader :family_name
-  attr_accessor :next_node
-  def initialize(family_name)
+  attr_accessor :next_node, :supply
+  def initialize(family_name, supplies_at_create)
     @family_name = family_name
     @next_node = nil
+    @supply = supplies_at_create
   end
 end
--- test/wagon_train_test.rb
+++ test/wagon_train_test.rb
@@ -13,7 +13,7 @@ class WagonTrainTest < MiniTest::Test
 
   def test_can_append_to_list
     wt = WagonTrain.new
-    wt.append("Burke")
+    wt.append("Burke", {"pounds of food" => 200})
 
     assert_equal "Burke", wt.list.head.family_name
     assert_equal 1, wt.count
@@ -21,11 +21,13 @@ class WagonTrainTest < MiniTest::Test
 
   def test_can_append_to_list_a_second_time
     wt = WagonTrain.new
-    wt.append("Burke")
-    wt.append("West")
+    wt.append("Burke", {"pounds of food" => 200})
+    wt.list.prepend("Hardy", {"spare wagon tongues" => 3})
+    wt.list.insert(1, "West", {"pounds of food" => 300})
 
     assert_equal "West", wt.list.head.next_node.family_name
-    assert_equal 2, wt.count
+    assert_equal 3, wt.count
+    assert_equal 500, wt.supplies["pounds of food"]
   end
 
 
