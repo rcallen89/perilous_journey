8b85c5ee69fc82b77f735c06cdea1ece1bd8a4d4
Ryan Allen <rcallen89@gmail.com>
2019-11-25 09:39:04 -0700

    Add SimpleCov test tool

--- test/node_test.rb
+++ test/node_test.rb
@@ -1,10 +1,11 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require_relative 'test_helper'
+# require 'minitest/autorun'
+# require 'minitest/pride'
 require './lib/node'
 
 class NodeTest < MiniTest::Test
   def test_node_exists_with_defaults
-    node = Node.new("Burke")
+    node = Node.new("Burke", {"pounds of meat" => 100})
 
     assert_instance_of Node, node
     assert_equal "Burke", node.family_name
